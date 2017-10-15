120ce0f21d36aaecb709a57d10e73e083b4d9d5c
sixGodChan <sunqim16@gmail.com>
2017-10-15 13:33:13 +0800

    区分popup方式以及正常方式

--- popup/app01/views.py
+++ popup/app01/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from app01 import models
 
 
@@ -13,9 +13,13 @@ def popup_add(request):
     if request.method == "GET":
         return render(request, 'popup_add.html')
     else:
-        popup_tag_id = request.GET.get('popup_id')  # 获取select字段id
-        print(popup_tag_id)
-        title = request.POST.get('title')
-        obj = models.UserGroup.objects.create(title=title)
-        context = {'popup_tag_id': popup_tag_id, 'option_id': obj.pk, 'option_text': obj.title}
-        return render(request, 'popup_response.html', context)
+        if request.GET.get('popup_id'):  # 如果时popup方式过来
+            popup_tag_id = request.GET.get('popup_id')  # 获取select字段id
+            title = request.POST.get('title')
+            obj = models.UserGroup.objects.create(title=title)
+            context = {'popup_tag_id': popup_tag_id, 'option_id': obj.pk, 'option_text': obj.title}
+            return render(request, 'popup_response.html', context)
+        else:
+            title = request.POST.get('title')
+            models.UserGroup.objects.create(title=title)
+            return HttpResponse('...')
